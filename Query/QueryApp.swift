790beef6fbda5736224c4ff58f8603695038acda
Sze En <seah.sze.en@dhs.edu.sg>
2026-08-06 23:35:58 +0800

    Initial Commit

---
+++ Query/QueryApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueryApp.swift
+//  Query
+//
+//  Created by T Krobot on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct QueryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
